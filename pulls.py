309471f041bf79d59add383077609e3e4c175b56
Rocked03 <rockedohthree@gmail.com>
2024-07-17 13:48:40 +1200

    remove duplicate text

--- pulls.py
+++ pulls.py
@@ -292,7 +292,7 @@ class PullsCog(commands.Cog, name="Pulls"):
             embeds[-1].set_footer(text=self.copyright[brand])
 
         embed = empty_embed.copy()
-        embed.set_footer(text=f"Data obtained by Rocked03 from {brand_links[brand]}.")
+        # embed.set_footer(text=f"Data obtained by Rocked03 from {brand_links[brand]}.")
         description = []
         if start:
             description.append(f"*Jump to the [beginning]({start.jump_url}).*")
